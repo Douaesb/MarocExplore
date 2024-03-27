85c2195686d21eadbc2829b07491dfa6ff17ee75
Douae sb <douae.sb411@example.com>
2024-03-27 15:50:12 +0000

    phpUnit test itineraireController

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use CategorySeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Categorie::create(['name' => 'Adventure']);
+        \App\Models\Categorie::create(['name' => 'Montagne']);
+        \App\Models\Categorie::create(['name' => 'Rivière']);
+        \App\Models\Categorie::create(['name' => 'Plage']);
+        \App\Models\Categorie::create(['name' => 'Monument']);
     }
 }
